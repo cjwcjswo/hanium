83a45e6ec306250fec81f6ec4d9525e9e3a5ede4
MSDN-SPECIAL\Administrator <hklk@naver.com>
2017-06-01 15:22:17 +0900

    채팅(미완성)

--- src/main/java/hanium/opo/handler/EchoHandler.java
+++ src/main/java/hanium/opo/handler/EchoHandler.java
@@ -1,7 +1,34 @@
 package hanium.opo.handler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class EchoHandler extends TextWebSocketHandler {
+	private List<WebSocketSession> connectUser;
+	public EchoHandler() {
+		connectUser = new ArrayList<WebSocketSession>();
+	}
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		System.out.println(session.getId()+"´Ô Á¢¼Ó");
+		connectUser.add(session);
+	}
+	
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		for(WebSocketSession webSocketSession:connectUser){
+			webSocketSession.sendMessage(new TextMessage(message.getPayload()));
+		}
+	}
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		System.out.println(session.getId()+"´Ô ÅðÀå");
+		connectUser.remove(session);
+	}
 
 }
